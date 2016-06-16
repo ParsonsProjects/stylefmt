59d72cef0fd9a96d7ee51808f21439d1f014bb83
Alan Parsons <alan.parsons@gforces.co.uk>
2016-06-16 14:53:09 +0100

    Fixes and amendments for @font-face comma separate list

--- index.js
+++ index.js
@@ -4,21 +4,22 @@ var scss = require('postcss-scss')
 var params = require('./lib/params')
 var formatAtRules = require('./lib/formatAtRules')
 var formatRules = require('./lib/formatRules')
+var formatDecls = require('./lib/formatDecls')
 var formatComments = require('./lib/formatComments')
 var formatSassVariables = require('./lib/formatSassVariables')
-
 
 var stylefmt = postcss.plugin('stylefmt', function (fullPath) {
   return function (root) {
     return params(fullPath).then(function (params) {
-      formatComments(root, params)
-      formatAtRules(root, params) // @FIX need to remove sass stuff
-      formatRules(root, params)
-      // formatSassVariables(root, params) @FIX !see above
-      return root
+    	formatDecls.formatDeclsSingle(root, params)
+      	formatComments(root, params)
+      	formatAtRules(root, params) // @FIX need to remove sass stuff
+      	formatRules(root, params)
+      	// formatSassVariables(root, params) @FIX !change to less
+      	return root
     }).catch(function (err) {
-      console.error(err.stack)
-      return root
+      	console.error(err.stack)
+      	return root
     })
   }
 })
